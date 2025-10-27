63adbb845ef0f93372d9364d04082a7bd9255a54
mandarin1977 <pkm30106@gmail.com>
2025-10-27 16:40:24 +0900

    sdfsdf

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
 // 페이지 컴포넌트들 import
@@ -36,7 +36,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
